d9ffe2cf58d98d5e50db1be021db9671b757388a
Dan Vanderkam <danvdk@gmail.com>
2024-12-23 15:57:56 -0500

    report perf

--- boggle.py
+++ boggle.py
@@ -2,6 +2,8 @@
 """Find all the words on a Boggle board using a C++ Trie."""
 
 import fileinput
+import sys
+import time
 from example import Trie
 
 
@@ -52,7 +54,6 @@ class Boggler:
     def score(self):
         self._score = 0
         self._runs += 1
-        self._used = [False] * 16
         t = self._trie
         for i in range(0, 16):
             c = self._cells[i]
@@ -85,10 +86,17 @@ def main():
     t = Trie.CreateFromFile("boggle-words.txt")
     b = Boggler(t)
     # print(f"Loaded {t.Size()} words")
+    start_s = time.time()
+    n = 0
     for line in fileinput.input():
         board = line.strip()
         b.set_board(board)
         print(f"{board}: {b.score()}")
+        n += 1
+    end_s = time.time()
+    elapsed_s = end_s - start_s
+    rate = n / elapsed_s
+    sys.stderr.write(f'{n} boards in {elapsed_s:.2f}s = {rate:.2f} boards/s\n')
 
 
 if __name__ == '__main__':
