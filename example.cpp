7d8c9d0d1903bdced95bb5e78619862dd0d77197
Dan Vanderkam <danvdk@gmail.com>
2024-12-23 14:55:39 -0500

    add a few more methods

--- example.cpp
+++ example.cpp
@@ -30,6 +30,11 @@ PYBIND11_MODULE(example, m)
 
     py::class_<Trie>(m, "Trie")
         // .def(py::init())
+        .def("StartsWord", &Trie::StartsWord)
+        .def("Descend", &Trie::Descend, py::return_value_policy::reference)
+        .def("IsWord", &Trie::IsWord)
+        .def("Mark", static_cast<uintptr_t (Trie::*)()>(&Trie::Mark))
+        .def("SetMark", static_cast<void (Trie::*)(uintptr_t)>(&Trie::Mark))
         // Possible that these should be ::reference_internal instead
         // See https://pybind11.readthedocs.io/en/stable/advanced/functions.html#return-value-policies
         .def("AddWord", &Trie::AddWord, py::return_value_policy::reference)
