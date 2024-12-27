551a8fd9a979cff3097d707fcb1faebc33640df1
Dan Vanderkam <danvdk@gmail.com>
2024-12-27 15:15:21 -0500

    symmetries

--- symmetry.py
+++ symmetry.py
@@ -10,3 +10,34 @@ def rot90[T](mat: list[list[T]]):
 def flip_x[T](mat: list[list[T]]):
     """Flip an NxM matrix along the x-axis."""
     return mat[::-1]
+
+
+def flip_y[T](mat: list[list[T]]):
+    """Flip an NxM matrix along the y-axis."""
+    return [row[::-1] for row in mat]
+
+
+def all_symmetries[T](mat: list[list[T]], no_rotations=False):
+    """Return all symmetries of a 2D matrix."""
+    fy = flip_y(mat)
+    flips = [
+        flip_x(mat),
+        fy,
+        flip_x(fy),
+    ]
+    if no_rotations:
+        return flips
+    r90 = rot90(mat)
+    r90fy = flip_y(r90)
+    return [
+        *flips,
+        r90,
+        flip_x(r90),
+        r90fy,
+        flip_x(r90fy)
+    ]
+
+
+def mat_to_str[T](mat: list[list[T]]):
+    """Convert a 2D matrix to a string."""
+    return '\n'.join([' '.join(map(str, row)) for row in mat])
--- symmetry_test.py
+++ symmetry_test.py
@@ -1,4 +1,4 @@
-from symmetry import flip_x, rot90
+from symmetry import all_symmetries, flip_x, flip_y, mat_to_str, rot90
 
 def test_rot90():
     mat = [
@@ -41,3 +41,50 @@ def test_flip_x():
         [1, 2, 3],
     ]
     assert flip_x(mat) == expected
+
+
+def test_flip_y():
+    mat = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+    ]
+    expected = [
+        [3, 2, 1],
+        [6, 5, 4],
+        [9, 8, 7],
+    ]
+    assert flip_y(mat) == expected
+
+
+def test_eightfold_sym():
+    mat = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+    ]
+    syms = all_symmetries(mat)
+    assert len(syms) == 7
+    unique = {mat_to_str(sym) for sym in [mat] + syms}
+    assert len(unique) == 8
+
+
+def test_fourfold_sym():
+    mat = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [10, 11, 12],
+    ]
+    syms8 = all_symmetries(mat)
+    assert len(syms8) == 7
+    unique = {mat_to_str(sym) for sym in [mat] + syms8}
+    assert len(unique) == 8
+
+    syms4 = all_symmetries(mat, no_rotations=True)
+    assert len(syms4) == 3
+    unique = {mat_to_str(sym) for sym in [mat] + syms4}
+    assert len(unique) == 4
+    for sym in syms4:
+        assert len(sym) == 4
+        assert len(sym[0]) == 3
