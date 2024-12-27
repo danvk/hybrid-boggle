c26cea31af95cd3a8385efe90d9d6f10f4d5e902
Dan Vanderkam <danvdk@gmail.com>
2024-12-27 17:57:43 -0500

    canonicalize script

---
+++ canonicalize.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+"""Exchange a list of boards for their canonically-rotated variants."""
+
+import fileinput
+import math
+
+from symmetry import canonicalize
+
+
+def list_to_matrix(letters):
+    n = math.sqrt(len(letters))
+    assert n == int(n)
+    n = int(n)
+    bd_2d = [[0 for _x in range(0, n)] for _y in range(0, n)]
+    for i in range(0, len(letters)):
+        bd_2d[i//n][i%n] = letters[i]
+    return bd_2d
+
+
+def main():
+    for line in fileinput.input():
+        bd = canonicalize(list_to_matrix(line.strip()))
+        print(''.join(str(x) for row in bd for x in row))
+
+
+if __name__ == "__main__":
+    main()
