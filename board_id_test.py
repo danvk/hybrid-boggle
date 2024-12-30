2b61c65ac11ee5ce6e515b43431157d040a85379
Dan Vanderkam <danvdk@gmail.com>
2024-12-30 11:47:18 -0500

    start board_id_tests

---
+++ board_id_test.py
@@ -0,0 +1,17 @@
+from board_id import board_id, from_board_id, is_canonical_board_id
+
+
+def test_board_id_round_trip():
+    classes = ['a', 'b', 'c', 'd']
+    board = from_board_id(classes, (3, 3), 0)
+    assert board == 'a a a a a a a a a'
+    assert board_id([[0, 0, 0], [0, 0, 0], [0, 0, 0]], (3, 3), 4) == 0
+
+    board = from_board_id(classes, (3, 3), 100068)
+    assert board == 'a b c d c b a c b'
+    assert board_id([[0, 1, 2], [3, 2, 1], [0, 2, 1]], (3, 3), 4) == 100068
+
+
+def test_is_canonical_board_id():
+    assert is_canonical_board_id(4, (3, 3), 0)
+    assert not is_canonical_board_id(4, (3, 3), 100068)
