15ec86ab20a193c0a493f9600c4fee41398a6993
Dan Vanderkam <danvdk@gmail.com>
2025-01-03 12:27:22 -0500

    tests for max/default

--- boggle/ibuckets_tree_test.py
+++ boggle/ibuckets_tree_test.py
@@ -13,7 +13,7 @@ def test_max_tree_ops_no_default():
     scalar3 = 3
     tree = MaxTree(cell=0, choices={"a": 1, "b": 2})
     low_tree = MaxTree(cell=0, choices={"a": 1, "b": 1})
-    abc_tree = MaxTree(cell=0, choices={"a": 0, "b": 7, "c": 3})
+    abc_tree = MaxTree(cell=0, choices={"a": 1, "b": 7, "c": 3})
 
     assert add_max_trees(scalar, tree) == MaxTree(
         cell=0, choices={"a": 3, "b": 4}, default=2
@@ -32,11 +32,13 @@ def test_max_tree_ops_no_default():
     assert max_of_max_trees(scalar, tree) == 2
     assert max_of_max_trees(tree, scalar3) == 3
     assert max_of_max_trees(scalar, abc_tree) == MaxTree(
-        cell=0, choices={"b": 7, "c": 3}, default=2
+        cell=0,
+        choices={"b": 7, "c": 3},
+        default=2,  # absorbs "a"
     )
     assert max_of_max_trees(low_tree, tree) == tree
     assert max_of_max_trees(tree, abc_tree) == MaxTree(
-        cell=0, choices={"a": 1, "b": 7, "c": 3}
+        cell=0, choices={"a": 2, "b": 7, "c": 3}
     )
     assert max_of_max_trees(scalar3, scalar) == 3
 
