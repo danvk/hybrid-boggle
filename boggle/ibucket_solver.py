1cd813b964bcaa54132f1ff4d74a010228d693c4
Dan Vanderkam <danvdk@gmail.com>
2025-01-03 11:21:57 -0500

    try all single forces; Python ibuckets is ~35x slower than C++

--- boggle/ibucket_solver.py
+++ boggle/ibucket_solver.py
@@ -18,12 +18,12 @@ class Timer:
         return self
 
     def __exit__(self, type, value, traceback):
-        self.time = (time.perf_counter() - self.start) * 1e6
-        self.readout = f"{self.label}: {self.time:.3f} µs"
+        self.time = (time.perf_counter() - self.start) * 1e3
+        self.readout = f"{self.label}: {self.time:.3f} ms"
         print(self.readout)
 
 
-def main():
+def main_old():
     (board,) = sys.argv[1:]
     t = Trie.CreateFromFile("boggle-words.txt")
     assert t.FindWord("qinqennia") is not None
@@ -48,6 +48,47 @@ def main():
         print(pbb.UpperBound(500_000))
     d = pbb.details_
     print(d.max_nomark, d.sum_union)
+
+
+def try_all(bb):
+    print(bb.as_string())
+    with Timer("root"):
+        score = bb.UpperBound(500_000)
+        d = bb.Details()
+        print("root", score, d.max_nomark, d.sum_union)
+
+    cells = bb.as_string().split(" ")
+    for i, cell in enumerate(cells):
+        if len(cell) == 1:
+            continue
+        max_cell = 0
+        with Timer(f"force {i}"):
+            for c in cell:
+                cp = [*cells]
+                cp[i] = c
+                assert bb.ParseBoard(" ".join(cp))
+                score = bb.UpperBound(500_000)
+                d = bb.Details()
+                print(f"{i}/{c}", score, d.max_nomark, d.sum_union)
+                max_cell = max(max_cell, score)
+        print(f"{i}: {max_cell}")
+
+
+def main():
+    (board,) = sys.argv[1:]
+    t = Trie.CreateFromFile("boggle-words.txt")
+    bb = BucketBoggler33(t)
+    bb.ParseBoard(board)
+    print("C++")
+    try_all(bb)
+
+    print("---")
+
+    pyt = make_py_trie("boggle-words.txt")
+    pbb = PyBucketBoggler(pyt)
+    pbb.ParseBoard(board)
+    print("Python")
+    try_all(pbb)
 
 
 if __name__ == "__main__":
