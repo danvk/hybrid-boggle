83bfedd1fe5b36200f0fa2bff0f1c63a61ab2272
Dan Vanderkam <danvdk@gmail.com>
2025-01-03 12:28:05 -0500

    tests passing

--- boggle/ibuckets_tree_test.py
+++ boggle/ibuckets_tree_test.py
@@ -38,7 +38,7 @@ def test_max_tree_ops_no_default():
     )
     assert max_of_max_trees(low_tree, tree) == tree
     assert max_of_max_trees(tree, abc_tree) == MaxTree(
-        cell=0, choices={"a": 2, "b": 7, "c": 3}
+        cell=0, choices={"a": 1, "b": 7, "c": 3}
     )
     assert max_of_max_trees(scalar3, scalar) == 3
 
