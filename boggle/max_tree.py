844705a994896ed9093e8708ff3653fc342b1acf
Dan Vanderkam <danvdk@gmail.com>
2025-01-03 16:01:02 -0500

    this might be doing something useful

--- boggle/max_tree.py
+++ boggle/max_tree.py
@@ -16,20 +16,39 @@ def add_max_trees(a: TreeOrScalar, b: TreeOrScalar) -> TreeOrScalar:
             return a + b
         return MaxTree(
             cell=b.cell,
-            choices={k: a + v for k, v in b.choices.items()},
+            choices={k: add_max_trees(a, v) for k, v in b.choices.items()},
         )
     else:
         if type(b) is int:
             return add_max_trees(b, a)
-        assert a.cell == b.cell
-        ac = a.choices
-        bc = b.choices
-        # assert set(ac.keys()) == set(bc.keys())
-        # It's unlikely that these all reduce to the same value
-        return MaxTree(
-            cell=a.cell,
-            choices={k: av + bc[k] for k, av in ac.items()},
-        )
+        if a.cell == b.cell:
+            ac = a.choices
+            bc = b.choices
+            # assert set(ac.keys()) == set(bc.keys())
+            # It's unlikely that these all reduce to the same value
+            return MaxTree(
+                cell=a.cell,
+                choices={k: add_max_trees(av, bc[k]) for k, av in ac.items()},
+            )
+        else:
+            if a.cell > b.cell:
+                a, b = b, a
+            # TODO: prune. Could be faster, too!
+            # TODO: cache a.choices[ac] lookup
+            return MaxTree(
+                cell=a.cell,
+                choices={
+                    ac: MaxTree(
+                        cell=b.cell,
+                        choices={
+                            bc: get_value(a, {a.cell: ac, b.cell: bc})
+                            + get_value(b, {a.cell: ac, b.cell: bc})
+                            for bc in b.choices.keys()
+                        },
+                    )
+                    for ac in a.choices.keys()
+                },
+            )
 
 
 def max_of_max_trees(a: TreeOrScalar, b: TreeOrScalar) -> TreeOrScalar:
@@ -40,25 +59,95 @@ def max_of_max_trees(a: TreeOrScalar, b: TreeOrScalar) -> TreeOrScalar:
             return a
         mt = MaxTree(
             cell=b.cell,
-            choices={k: max(a, v) for k, v in b.choices.items()},
+            choices={k: max_of_max_trees(a, v) for k, v in b.choices.items()},
         )
-        assert mt.choices
         return mt
     else:
         if type(b) is int:
             return max_of_max_trees(b, a)
-        assert a.cell == b.cell
-        ac = a.choices
-        bc = b.choices
-        # assert set(ac.keys()) == set(bc.keys())
-        # It's unlikely that these all reduce to the same value
-        return MaxTree(
-            cell=a.cell,
-            choices={k: max(av, bc[k]) for k, av in ac.items()},
-        )
+        if a.cell == b.cell:
+            ac = a.choices
+            bc = b.choices
+            # assert set(ac.keys()) == set(bc.keys())
+            # It's unlikely that these all reduce to the same value
+            return MaxTree(
+                cell=a.cell,
+                choices={k: max_of_max_trees(av, bc[k]) for k, av in ac.items()},
+            )
+        else:
+            if a.cell > b.cell:
+                a, b = b, a
+            # TODO: prune. Could be faster, too!
+            # TODO: cache a.choices[ac] lookup
+            return MaxTree(
+                cell=a.cell,
+                choices={
+                    ac: MaxTree(
+                        cell=b.cell,
+                        choices={
+                            bc: max(
+                                get_value(a, {a.cell: ac, b.cell: bc}),
+                                get_value(b, {a.cell: ac, b.cell: bc}),
+                            )
+                            for bc in b.choices.keys()
+                        },
+                    )
+                    for ac in a.choices.keys()
+                },
+            )
 
 
 def max_tree_max(t: TreeOrScalar) -> int:
     if type(t) is int:
         return t
-    return max(t.choices.values())
+    return max(max_tree_max(child) for child in t.choices.values())
+
+
+def get_value(t: TreeOrScalar, choices: dict[int, str]) -> int:
+    if type(t) is int:
+        return t
+    cell_choice = choices.get(t.cell)
+    if cell_choice is not None:
+        subtree = t.choices[cell_choice]
+        return get_value(subtree, choices)
+    # if we're unconstrained, take the max of all choices
+    return max(get_value(t, choices) for t in t.choices.values())
+
+
+def pivot(t: MaxTree) -> MaxTree:
+    """Flip the order of a depth-two MaxTree."""
+    cell1s = [
+        *set(child.cell for child in t.choices.values() if isinstance(child, MaxTree))
+    ]
+    assert len(cell1s) == 1
+    choice1s = set(
+        cell1
+        for child in t.choices.values()
+        if isinstance(child, MaxTree)
+        for cell1 in child.choices.keys()
+    )
+
+    # TODO: this could be more efficient
+    cell1 = cell1s[0]
+    tree = MaxTree(
+        cell=cell1,
+        choices={
+            c1: MaxTree(
+                cell=t.cell,
+                choices={
+                    c0: get_value(t, {t.cell: c0, cell1: c1}) for c0 in t.choices.keys()
+                },
+            )
+            for c1 in choice1s
+        },
+    )
+
+    # Collapse useless nodes. Unclear whether this is helpful in practice,
+    # but it does give an invariant that pivot(pivot(X)) == X.
+    for c1, child in tree.choices.items():
+        vs = child.choices.values()
+        minv = min(vs)
+        if minv == max(vs):
+            tree.choices[c1] = minv
+
+    return tree
--- boggle/max_tree_test.py
+++ boggle/max_tree_test.py
@@ -1,8 +1,10 @@
 from boggle.max_tree import (
     MaxTree,
     add_max_trees,
+    get_value,
     max_of_max_trees,
     max_tree_max,
+    pivot,
 )
 
 
@@ -42,3 +44,38 @@ def test_max_tree_ops_no_default():
         cell=0, choices={"a": 1, "b": 7, "c": 3}
     )
     assert max_of_max_trees(scalar3, scalar) == 3
+
+
+def test_depth_two_tree():
+    scalar = 2
+    tree_cell1 = MaxTree(cell=1, choices={"c": 4, "d": 5})
+    tree_cell0 = MaxTree(cell=0, choices={"a": 3, "b": 4, "c": tree_cell1})
+
+    assert max_tree_max(tree_cell0) == 5
+
+    assert get_value(tree_cell0, {0: "a"}) == 3
+    assert get_value(tree_cell0, {0: "b"}) == 4
+    assert get_value(tree_cell0, {0: "c"}) == 5  # max of tree_cell1
+    assert get_value(tree_cell0, {0: "c", 1: "c"}) == 4
+    assert get_value(tree_cell0, {0: "c", 1: "d"}) == 5
+
+    pivoted = pivot(tree_cell0)
+    assert get_value(pivoted, {0: "a"}) == 3
+    assert get_value(pivoted, {0: "b"}) == 4
+    assert get_value(pivoted, {0: "c"}) == 5  # max of tree_cell1
+    assert get_value(pivoted, {0: "c", 1: "c"}) == 4
+    assert get_value(pivoted, {0: "c", 1: "d"}) == 5
+
+    back = pivot(pivoted)
+    # print(back)
+    assert back == tree_cell0
+
+    sum_tree = add_max_trees(tree_cell0, pivoted)
+    assert get_value(sum_tree, {0: "a"}) == 2 * 3
+    assert get_value(sum_tree, {0: "b"}) == 2 * 4
+    assert get_value(sum_tree, {0: "c"}) == 2 * 5  # max of tree_cell1
+    assert get_value(sum_tree, {0: "c", 1: "c"}) == 2 * 4
+    assert get_value(sum_tree, {0: "c", 1: "d"}) == 2 * 5
+
+    mistree = add_max_trees(tree_cell0, tree_cell1)
+    assert get_value(mistree, {0: "a"}) == 8
