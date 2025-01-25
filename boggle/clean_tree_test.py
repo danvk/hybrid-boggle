9945e3b461ff4093da80a561b074a37ed5277ac9
Dan Vanderkam <danvdk@gmail.com>
2025-01-25 11:25:17 -0500

    lift_choice unit tests pass

---
+++ boggle/clean_tree_test.py
@@ -0,0 +1,66 @@
+from boggle.clean_tree import ChoiceNode, SumNode, TreeBuilder, eval_all, lift_choice
+from boggle.trie import make_py_trie
+
+
+def test_lift_sum():
+    root = SumNode(
+        points=0,
+        children=[
+            ChoiceNode(cell=1, children=[1, 3]),
+            ChoiceNode(cell=1, children=[2, 4]),
+        ],
+    )
+    assert lift_choice(root, 0, 1) == root
+    assert lift_choice(root, 1, 2) == ChoiceNode(
+        cell=1,
+        children=[
+            # TODO: these should collapse to ints
+            SumNode(
+                points=0,
+                children=[1, 2],
+            ),
+            SumNode(points=0, children=[3, 4]),
+        ],
+    )
+
+
+def test_lift_choice():
+    root = ChoiceNode(
+        cell=0,
+        children=[
+            ChoiceNode(cell=1, children=[1, 3]),
+            ChoiceNode(cell=1, children=[2, 4]),
+        ],
+    )
+    assert lift_choice(root, 0, 2) == root
+    assert lift_choice(root, 2, 1) == root
+    assert lift_choice(root, 1, 2) == ChoiceNode(
+        cell=1,
+        children=[
+            ChoiceNode(cell=0, children=[1, 2]),
+            ChoiceNode(cell=0, children=[3, 4]),
+        ],
+    )
+
+
+def test_lift_invariants_22():
+    trie = make_py_trie("boggle-words-4.txt")
+    board = "ny ae ch ."
+    cells = board.split(" ")
+    etb = TreeBuilder(trie, dims=(2, 2))
+    t = etb.build_tree(board)
+
+    scores = eval_all(t, cells)
+
+    # Try lifting each cell; this should not affect any scores.
+    for i, cell in enumerate(cells):
+        if len(cell) <= 1:
+            continue
+        tl = lift_choice(t, i, len(cell))
+        lift_scores = eval_all(tl, cells)
+        assert lift_scores == scores
+
+
+# TODO:
+# - test invariant that sum nodes have no children that are sum nodes or ints
+# - test lift invariant on 3x3 board: '. . . . lnrsy e aeiou aeiou .'
