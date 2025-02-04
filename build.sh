d3512de641a4be84be1f2fcf29bb34f6165dd96a
Dan Vanderkam <danvdk@gmail.com>
2025-02-04 14:58:15 -0500

    Add Dockerfile (#9)
    * working on Dockerfile
    
    * tie -undefined dynamic_lookup to Apple Clang
    
    * try that again
    
    * fill out Docker instructions
    
    * . not source

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-set -o errexit
 
+c++ --version | grep Apple > /dev/null
+if [[ $? == 0 ]]; then
+    EXTRA_FLAGS="-undefined dynamic_lookup"
+else
+    EXTRA_FLAGS=""
+fi
+
+set -o errexit
 cd cpp
 c++ -Wall -shared -std=c++20 -fPIC -march=native \
     -Wno-sign-compare \
@@ -8,7 +15,7 @@ c++ -Wall -shared -std=c++20 -fPIC -march=native \
     $(poetry run python -m pybind11 --includes) \
     cpp_boggle.cc trie.cc eval_node.cc \
     -o ../cpp_boggle$(python3-config --extension-suffix) \
-    -undefined dynamic_lookup
+    $EXTRA_FLAGS
 
 # TODO: fix the sign-compare warnings
 # For sampling profiling:
