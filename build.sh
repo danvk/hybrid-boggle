a79ed217616c441548aec38fa00a45f204ea3803
Dan Vanderkam <danvdk@gmail.com>
2025-02-04 09:24:54 -0500

    Turn off sign compare warnings for now

--- build.sh
+++ build.sh
@@ -3,12 +3,14 @@ set -o errexit
 
 cd cpp
 c++ -Wall -shared -std=c++20 -fPIC -march=native \
+    -Wno-sign-compare \
     -O3 \
     $(poetry run python -m pybind11 --includes) \
     cpp_boggle.cc trie.cc eval_node.cc \
     -o ../cpp_boggle$(python3-config --extension-suffix) \
     -undefined dynamic_lookup
 
+# TODO: fix the sign-compare warnings
 # For sampling profiling:
 #     -g
 
