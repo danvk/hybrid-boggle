1ca1b2e1e0d71373c29c87bf68d19ecb11415691
Dan Vanderkam <danvdk@gmail.com>
2025-02-07 15:46:08 -0500

    OR Tools POC

--- boggle/z3.py
+++ boggle/z3.py
@@ -12,7 +12,7 @@ from boggle.trie import PyTrie, make_lookup_table, make_py_trie
 MARK = 2
 
 
-def collect_equations(t: EvalNode, cells, eqs: list):
+def collect_equations(t: EvalNode, cells, eqs: list, id_to_node):
     if t.cache_key == MARK:
         return t.cache_value
 
@@ -23,7 +23,7 @@ def collect_equations(t: EvalNode, cells, eqs: list):
         for c in t.children:
             if c:
                 letter_id = f"{cells[c.cell][c.letter]}{c.cell}"
-                child_id = collect_equations(c, cells, eqs)
+                child_id = collect_equations(c, cells, eqs, id_to_node)
                 choices.append((letter_id, child_id))
         if len(choices) == 1 and choices[0][1] == 1:
             t.cache_key = MARK
@@ -36,7 +36,7 @@ def collect_equations(t: EvalNode, cells, eqs: list):
         terms = [t.points] if t.points else []
         for c in t.children:
             if c:
-                child_id = collect_equations(c, cells, eqs)
+                child_id = collect_equations(c, cells, eqs, id_to_node)
                 terms.append(child_id)
         if len(terms) > 1:
             me_id = f"n_{len(eqs)}"
@@ -46,6 +46,7 @@ def collect_equations(t: EvalNode, cells, eqs: list):
             t.cache_value = terms[0]
             return terms[0]
 
+    id_to_node[me_id] = t
     t.cache_key = MARK
     t.cache_value = me_id
     return me_id
@@ -83,6 +84,84 @@ def to_cmsat(cells, best_score: int, root_id, eqs):
     print(f"(assert (> {root_id} {best_score}))")
     print("(check-sat)")
     # print("(get-model)")
+
+
+def to_ortools(cells, best_score: int, root_id, eqs, eq_id_to_node):
+    print("from ortools.sat.python import cp_model")
+    print("model = cp_model.CpModel()")
+    print("cells = []")
+    for i, cell in enumerate(cells):
+        vars = []
+        vars_dict = {}
+        for letter in cell:
+            var = f"{letter}{i}"
+            vars.append(var)
+            vars_dict[letter] = var
+            print(f"{var} = model.new_bool_var('{var}')")
+        if len(vars) > 1:
+            # print(f"model.add({'+'.join(vars)} == 1)")
+            print(f"model.AddExactlyOne({', '.join(vars)})")
+        vd = ",".join(f'"{let}": {v}' for let, v in vars_dict.items())
+        print("cells.append({%s})" % vd)
+
+    # (a, b) -> equation for a*b
+    product_eqs = {}
+
+    for eq_id, eq_type, terms in eqs:
+        n = eq_id_to_node[eq_id]
+        if eq_type == "sum":
+            eq = "+".join(str(t) for t in terms)
+        else:
+            products = []
+            for letter_id, term_id in terms:
+                if term_id == 1:
+                    products.append(letter_id)
+                else:
+                    key = (
+                        (letter_id, term_id)
+                        if letter_id < term_id
+                        else (term_id, letter_id)
+                    )
+                    prod_id = product_eqs.get(key)
+                    if prod_id is None:
+                        prod_id = f"p_{len(product_eqs)}"
+                        product_eqs[key] = prod_id
+                        term_node = eq_id_to_node.get(term_id)
+                        if term_node:
+                            bound = term_node.bound
+                        else:
+                            bound = 1
+                        print(f"{prod_id} = model.new_int_var(0, {bound}, '{prod_id}')")
+                        print(
+                            f"model.add_multiplication_equality({prod_id}, ({letter_id}, {term_id}))"
+                        )
+                    # TODO: if len(products) == 1, can simplify
+                    products.append(prod_id)
+            eq = "+".join(products)
+        print(f"{eq_id} = model.new_int_var(0, {n.bound}, '{eq_id}')")
+        print(f"model.add({eq_id} == {eq})")
+
+    print(f"model.add({root_id} >= {best_score})")
+    print(
+        """
+solver = cp_model.CpSolver()
+status = solver.solve(model)
+
+status = {
+    cp_model.OPTIMAL: "optimal",
+    cp_model.FEASIBLE: "feasible",
+    cp_model.INFEASIBLE: "infeasible",
+    cp_model.MODEL_INVALID: "model_invalid",
+    cp_model.UNKNOWN: "unknown",
+}[status]
+print(status)
+print('value:', solver.value(%s))
+
+letters = [letter for cell in cells for letter, v in cell.items() if solver.value(v)]
+print(" ".join(letters))
+"""
+        % root_id
+    )
 
 
 def to_py(cells, root_id, eqs):
@@ -135,12 +214,14 @@ def main():
     assert etb.ParseBoard(board)
     tree = etb.BuildTree(None, dedupe=True)
     eqs = []
-    root_id = collect_equations(tree, cells, eqs)
+    eq_id_to_node = {}
+    root_id = collect_equations(tree, cells, eqs, eq_id_to_node)
     sys.stderr.write(f"eq count: {len(eqs)}\n")
     # print(eqs)
     # print("---\n")
-    to_cmsat(cells, cutoff, root_id, eqs)
+    # to_cmsat(cells, cutoff, root_id, eqs)
     # to_py(cells, root_id, eqs)
+    to_ortools(cells, cutoff, root_id, eqs, eq_id_to_node)
 
 
 if __name__ == "__main__":
