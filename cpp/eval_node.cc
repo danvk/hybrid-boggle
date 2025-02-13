1ae714a68d30e50b8cc3a10133934acfb39d0eb3
Dan Vanderkam <danvdk@gmail.com>
2025-02-13 10:12:06 -0500

    back-port bugfix to C++

--- cpp/eval_node.cc
+++ cpp/eval_node.cc
@@ -489,11 +489,20 @@ bool SqueezeSumNodeInPlace(EvalNode* node, EvalNodeArena& arena, bool should_mer
   // There's something to absorb.
   // TODO: pre-reserve the right number of slots for new_children
   auto& new_children = choice;
+  uint32_t new_bound = 0;
+  for (auto c : choice) {
+    new_bound += c->bound_;
+  }
+  uint32_t new_points_from_children = 0;
   for (auto c : non_choice) {
-    node->points_ += c->points_;
+    new_points_from_children += c->points_;
     new_children.insert(new_children.end(), c->children_.begin(), c->children_.end());
+    new_bound += c->bound_;
   }
   node->children_.swap(new_children);
+  // We need to take care here not to double-count points for the bound.
+  node->bound_ = new_bound + node->points_;
+  node->points_ += new_points_from_children;
   return true;
 }
 
