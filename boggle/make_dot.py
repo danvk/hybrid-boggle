dec08762e6cadf2ee2747271eee7b20bafcc03f6
Dan Vanderkam <danvdk@gmail.com>
2025-02-21 12:55:36 -0500

    Colorful/minimal DOT renderings (#40)
    * natural tree example
    
    * colorful DOT
    
    * compress; these are looking pretty good!
    
    * elide uninteresting sum nodes
    
    * nodesep
    
    * orderly
    
    * flags for make_dot
    
    * tighten nodes
    
    * ruffen

--- boggle/make_dot.py
+++ boggle/make_dot.py
@@ -1,45 +1,70 @@
 #!/usr/bin/env python
 
 
+import argparse
 import sys
 
+from boggle.args import add_standard_args
 from boggle.dimensional_bogglers import LEN_TO_DIMS
-from boggle.eval_tree import TreeBuilder
+from boggle.orderly_tree_builder import OrderlyTreeBuilder
+from boggle.tree_builder import TreeBuilder
 from boggle.trie import make_py_trie
 
 
 def main():
-    trie = make_py_trie("wordlists/enable2k.txt")
-    (board, *lift_cell_strs) = sys.argv[1:]
-    lift_cells = [int(s) for s in lift_cell_strs]
+    parser = argparse.ArgumentParser(
+        prog="DOT renderer",
+        description="Visualize what's going on with those trees.",
+    )
+    # TODO: don't set size, we just need the dictionary
+    add_standard_args(parser)
+    parser.add_argument(
+        "--tree_builder",
+        choices=("natural", "orderly"),
+        default="orderly",
+        help="Tree builder to use.",
+    )
+    parser.add_argument(
+        "--compress", action="store_true", help="Compress EvalTree while lifting"
+    )
+    parser.add_argument("board", type=str, help="Board class to render.")
+    parser.add_argument("lift_cells", nargs="*", help="Sequence of cells to lift")
+    args = parser.parse_args()
+    trie = make_py_trie(args.dictionary)
+    board = args.board
+    lift_cells = [int(s) for s in args.lift_cells]
 
     cells = board.split(" ")
     dims = LEN_TO_DIMS[len(cells)]
-    etb = TreeBuilder(trie, dims)
+    if args.tree_builder == "natural":
+        etb = TreeBuilder(trie, dims)
+    else:
+        etb = OrderlyTreeBuilder(trie, dims)
     etb.ParseBoard(board)
-    t = etb.BuildTree(dedupe=True)
+    t = etb.BuildTree()  # dedupe=False)
     # assert_invariants(t, cells)
     # dedupe_subtrees(t)
 
     mark = 1
 
-    mark += 1
-    sys.stderr.write(
-        f"node count: {t.node_count()}, uniq={t.unique_node_count(mark)} bound={t.bound}\n"
-    )
     # t0 = t.children[0]
     # t0t = t0.children[0]
     # t = t0t
 
     with open("tree.dot", "w") as out:
         # out.write(t.to_dot(cells, max_depth=2))
-        out.write(t.to_dot(cells))
+        out.write(t.to_dot(cells, trie=trie))
         out.write("\n")
+
+    mark += 1
+    sys.stderr.write(
+        f"tree.dot node count: {t.node_count()}, uniq={t.unique_node_count(mark)} bound={t.bound}\n"
+    )
 
     for i, cell in enumerate(lift_cells):
         mark += 1
         t = t.lift_choice(
-            cell, len(cells[cell]), None, mark, dedupe=True, compress=False
+            cell, len(cells[cell]), None, mark, dedupe=False, compress=True
         )
         mark += 1
         sys.stderr.write(
