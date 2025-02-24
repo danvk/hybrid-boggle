3d4d0f9c5a0954d15e1ad1a2692be650b1cd516a
Dan Vanderkam <danvdk@gmail.com>
2025-02-24 11:44:43 -0500

    add back reverse lookup

--- cpp/cpp_boggle.cc
+++ cpp/cpp_boggle.cc
@@ -79,7 +79,7 @@ PYBIND11_MODULE(cpp_boggle, m)
         .def("Size", &Trie::Size)
         .def("NumNodes", &Trie::NumNodes)
         .def("SetAllMarks", &Trie::SetAllMarks)
-        // .def_static("ReverseLookup", py::overload_cast<const Trie*, const Trie*>(&Trie::ReverseLookup))
+        .def_static("ReverseLookup", py::overload_cast<const Trie*, const Trie*>(&Trie::ReverseLookup))
         .def_static("CreateFromFile", &Trie::CreateFromFile)
         .def_static("CreateFromFileWithGrouping", &Trie::CreateFromFileWithGrouping);
 
