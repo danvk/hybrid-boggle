fe2cb984230955ac44a1142bb3a3b39d4b847560
Dan Vanderkam <danvdk@gmail.com>
2025-04-02 11:54:31 -0500

    Hillclimb improvements (#74)
    * print top N boards from hillclimbing
    
    * canonicalize as you go
    
    * multithreaded hill-climbing
    
    * report number of boards
    
    * dumb bug
    
    * lint

--- boggle/hillclimb.py
+++ boggle/hillclimb.py
@@ -12,12 +12,15 @@ https://en.wikipedia.org/wiki/Greedy_randomized_adaptive_search_procedure
 """
 
 import argparse
+import functools
+import multiprocessing
 import random
+import time
 from collections import Counter
 
 from boggle.anneal import initial_board
 from boggle.args import add_standard_args, get_trie_and_boggler_from_args
-from boggle.boggler import PyBoggler
+from boggle.symmetry import canonicalize, list_to_matrix
 from boggle.trie import LETTER_A
 
 
@@ -54,9 +57,32 @@ def neighbors(board: str):
     return out
 
 
-def hillclimb(boggler: PyBoggler, num_lets: int, args):
+def get_process_id():
+    ids = multiprocessing.current_process()._identity
+    if len(ids) == 0:
+        return 1  # single-threaded case
+    assert len(ids) == 1
+    return ids[0]
+
+
+def hillclimb(task: int):
+    me = get_process_id()
+    seed = hillclimb.random_seed + task
+    random.seed(seed)
+    print(f"#{me} starting hillclimb with random seed = {seed}")
+    args = hillclimb.args
+    w, h = hillclimb.dims
+    boggler = hillclimb.boggler
+    num_lets = w * h
+
+    @functools.cache
+    def canonicalize_str(board: str) -> str:
+        bd = canonicalize(list_to_matrix(board))
+        return "".join(str(x) for row in bd for x in row)
+
     pool = [
-        "".join(chr(x) for x in initial_board(num_lets)) for _ in range(args.pool_size)
+        canonicalize_str("".join(chr(x) for x in initial_board(num_lets)))
+        for _ in range(args.pool_size)
     ]
     score_cache = dict[str, int]()
     for bd in pool:
@@ -67,22 +93,36 @@ def hillclimb(boggler: PyBoggler, num_lets: int, args):
     num_iter = 0
     while True:
         num_iter += 1
-        ns = {n for seed in pool for n in neighbors(seed)}
-        scores = []
+        ns = {canonicalize_str(n) for seed in pool for n in neighbors(seed)}
+        scores: list[tuple[int, str]] = []
         for n in ns:
             score = score_cache.get(n) or boggler.score(n)
             score_cache[n] = score
             scores.append((score, n))
         scores.sort(reverse=True)
         scores = scores[: args.pool_size]
-        print(f"{num_iter=}: {max(scores)=} {min(scores)=}")
+        print(f"#{me} {num_iter=}: {max(scores)=} {min(scores)=}")
         new_pool = [bd for _, bd in scores]
         if new_pool == pool:
             break
         pool = new_pool
 
     best_score, best_bd = max(scores)
-    return best_score, best_bd, num_iter
+    return best_score, best_bd, num_iter, scores
+
+
+def hillclimb_init(args):
+    hillclimb.args = args
+    trie, boggler = get_trie_and_boggler_from_args(args)
+    hillclimb.trie = trie  # not used directly, but must not be GC'd
+    hillclimb.boggler = boggler
+    if args.random_seed >= 0:
+        hillclimb.random_seed = args.random_seed
+    else:
+        hillclimb.random_seed = time.time()
+
+    w, h = args.size // 10, args.size % 10
+    hillclimb.dims = (w, h)
 
 
 def main():
@@ -108,29 +148,40 @@ def main():
         default=100,
         help="Keep this many candidates as seeds for the next round.",
     )
+    parser.add_argument(
+        "--num_threads",
+        type=int,
+        default=1,
+        help="Number of concurrent hillclimbing runs to perform.",
+    )
     # TODO: character list
     add_standard_args(parser, random_seed=True, python=True)
 
     args = parser.parse_args()
+    pool = None
+    tasks = range(args.num_boards)
+    if args.num_threads > 1:
+        pool = multiprocessing.Pool(args.num_threads, hillclimb_init, (args,))
+        it = pool.imap_unordered(hillclimb, tasks)
+    else:
+        hillclimb_init(args)
+        it = (hillclimb(task) for task in tasks)
 
-    if args.random_seed >= 0:
-        random.seed(args.random_seed)
+    best = Counter[tuple[int, str]]()
+    for run, (score, board, n, score_boards) in enumerate(it):
+        print(f"{run}/{args.num_boards} {score} {board} ({n} iterations)")
+        best.update(score_boards)
 
-    w, h = args.size // 10, args.size % 10
-
-    _t, boggler = get_trie_and_boggler_from_args(args)
-
-    best = Counter[str]()
-    for run in range(args.num_boards):
-        score, board, n = hillclimb(boggler, w * h, args)
-        print(f"{run=} {score} {board} ({n} iterations)")
-        best[board] = score
-
-    if args.num_boards >= 10:
+    if args.num_boards > 1:
         print("---")
-        print("Top ten boards:")
-        for score, board in best.most_common(10):
-            print(f"{score} {board}")
+        print(f"Top {args.pool_size} boards:")
+        tops = [*best.keys()]
+        tops.sort(reverse=True)
+        tops = tops[: args.pool_size]
+        for score_board in tops:
+            score, board = score_board
+            freq = best[score_board]
+            print(f"{score}\t{board}\t{freq}/{args.num_boards}")
 
 
 if __name__ == "__main__":
