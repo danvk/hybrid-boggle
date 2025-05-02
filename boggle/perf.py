dcbfc738c4ba8eb6d276e7d9dd847816562455da
Dan Vanderkam <danvdk@gmail.com>
2025-05-02 08:43:08 -0500

    add --variations_on flag to perf script (#106)

--- boggle/perf.py
+++ boggle/perf.py
@@ -14,6 +14,7 @@ import time
 
 from boggle.args import add_standard_args, get_trie_and_boggler_from_args
 from boggle.boggler import LETTER_A
+from boggle.hillclimb import neighbors
 
 
 def random_board(n: int) -> str:
@@ -31,7 +32,19 @@ def main():
         type=str,
         help="Use this file instead of generating random boards.",
     )
-    parser.add_argument("num_boards", type=int, help="Number of boards to evaluate")
+    parser.add_argument(
+        "--variations_on",
+        type=str,
+        help="Generate 1- and 2-cell variations on this board. Useful for "
+        "testing performance on high-scoring boards.",
+    )
+    parser.add_argument(
+        "num_boards",
+        type=int,
+        help="Number of boards to evaluate",
+        default=100_000,
+        nargs="?",
+    )
     args = parser.parse_args()
     if args.random_seed >= 0:
         random.seed(args.random_seed)
@@ -40,14 +53,24 @@ def main():
     t, boggler = get_trie_and_boggler_from_args(args)
 
     w, h = args.size // 10, args.size % 10
-    if not args.input_file:
-        print(f"Generating {n} {w}x{h} boards...")
-        boards = [random_board(w * h) for _ in range(n)]
-    else:
+    if args.variations_on:
+        board = args.variations_on
+        assert len(board) == w * h
+        boards1 = neighbors(board)
+        boards = {bd for n1 in boards1 for bd in neighbors(n1)}
+        boards.update(boards1)
+        boards.add(board)
+        boards = [*boards]
+        print(f"Generated {len(boards)} neighbors of {board}")
+    elif args.input_file:
         boards = [line.strip() for line in open(args.input_file)]
         for board in boards:
             assert len(board) == w * h
         print(f"Read {len(boards)} boards from {args.input_file}")
+    else:
+        print(f"Generating {n} {w}x{h} boards...")
+        boards = [random_board(w * h) for _ in range(n)]
+
     total_score = 0
     print("Scoring boards...")
     start_s = time.time()
