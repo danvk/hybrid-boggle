ec9e12f1444454de60577750154827f6cf919aec
Dan Vanderkam <danvdk@gmail.com>
2025-05-15 11:35:20 -0500

    Use -isystem to disable warnings for pybind11 (#125)
    * use -isystem to disable warnings for pybind11
    
    * -Werror to make warnings errors
    
    * fix shadowing issue

--- build.sh
+++ build.sh
@@ -12,8 +12,9 @@ cd cpp
 c++ -Wall -shared -std=c++20 -fPIC -march=native \
     -Wno-sign-compare \
     -Wshadow \
+    -Werror \
     -O3 \
-    $(poetry run python -m pybind11 --includes) \
+    $(poetry run python -m pybind11 --includes | perl -pe 's/-I/-isystem /g') \
     cpp_boggle.cc trie.cc arena.cc eval_node.cc symmetry.cc \
     -o ../cpp_boggle$(python3-config --extension-suffix) \
     $EXTRA_FLAGS
