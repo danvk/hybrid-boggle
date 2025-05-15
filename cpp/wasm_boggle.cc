64495ac8a6c94ce9bdf17c1a5b30dea0dbe9ec64
Dan Vanderkam <danvdk@gmail.com>
2025-05-15 08:52:01 -0500

    Dimensional bogglers for WASM (#124)
    * dimensional bogglers for WASM
    
    * update target

--- cpp/wasm_boggle.cc
+++ cpp/wasm_boggle.cc
@@ -15,6 +15,12 @@ EMSCRIPTEN_BINDINGS(wasm_boggle) {
   register_vector<int>("VectorInt");
   register_vector<vector<int>>("VectorVectorInt");
 
-  using BB = Boggler<4, 4>;
-  class_<BB>("Boggler").constructor<Trie*>().function("find_words", &BB::FindWords);
+  using BB3 = Boggler<3, 3>;
+  class_<BB3>("Boggler33").constructor<Trie*>().function("find_words", &BB3::FindWords);
+
+  using BB4 = Boggler<4, 4>;
+  class_<BB4>("Boggler44").constructor<Trie*>().function("find_words", &BB4::FindWords);
+
+  using BB5 = Boggler<5, 5>;
+  class_<BB5>("Boggler55").constructor<Trie*>().function("find_words", &BB5::FindWords);
 }
---
+++ wasm/build-wasm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -o errexit
+
+emcc \
+    -lembind \
+    -sALLOW_MEMORY_GROWTH \
+    -sEXPORTED_FUNCTIONS=FS \
+    -sFORCE_FILESYSTEM=1 \
+    -o wasm/boggle.js \
+    --emit-tsd boggle-wasm.ts \
+    cpp/wasm_boggle.cc cpp/trie.cc
