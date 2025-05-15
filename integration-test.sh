917123c3042872a055ee8464438e6198c2abe684
Dan Vanderkam <danvdk@gmail.com>
2025-05-15 14:58:58 -0400

    update defaults

--- integration-test.sh
+++ integration-test.sh
@@ -5,6 +5,7 @@ poetry run python -m boggle.break_all \
     'bdfgjvwxz aeiou lnrsy chkmpt' 1400 \
     --size 34 \
     --board_id 2520743 \
+    --switchover_score 3500 \
     --log_per_board_stats \
     --omit_times \
     > testdata/3x4-2520743-1400.txt
