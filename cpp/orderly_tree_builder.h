b6b90eac6b536d02d9c10535fd0f69df6261d04b
Dan Vanderkam <danvdk@gmail.com>
2025-05-15 08:33:21 -0400

    drop logging

--- cpp/orderly_tree_builder.h
+++ cpp/orderly_tree_builder.h
@@ -89,8 +89,8 @@ const SumNode* OrderlyTreeBuilder<M, N>::BuildTree(EvalNodeArena& arena, bool de
   auto root = root_;
   root_ = NULL;
   dict_->ResetMarks();
-  cout << "len(found_word)=" << found_words_.size()
-       << ", num_overflow=" << num_overflow_ << endl;
+  // cout << "len(found_word)=" << found_words_.size()
+  //     << ", num_overflow=" << num_overflow_ << endl;
   found_words_.clear();
   unordered_set<pair<uintptr_t, uint64_t>, pair_hash<uintptr_t, uint64_t>>().swap(
       found_words_
