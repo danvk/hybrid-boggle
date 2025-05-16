1037257efa44279b2af780dd94ff8821e7cd8da7
Dan Vanderkam <danvdk@gmail.com>
2025-05-16 15:44:51 -0400

    68115 possible paths

--- boggle/hamiltonian.py
+++ boggle/hamiltonian.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
-"""Enumerate Hamiltonian circuits of the Boggle board."""
+"""Enumerate Hamiltonian circuits of the Boggle board.
+
+- 2x2: 6 possible paths
+- 2x3: 28 = 20 + 8 possible paths
+- 3x3: 220 = 138 + 50 + 32 possible paths
+- 3x4: 2757 = 1309 + 612 + 470 + 366
+- 4x4: 68115 = 37948 + 17681 + 12486
+"""
 
 from boggle.neighbors import NEIGHBORS
 
 
 def rec(cell: int, num_left: int, used: list[bool], seq: list[int], neighbors):
     if num_left == 0:
-        print(seq)
+        # print(seq)
         return 1  # success! a hamiltonian circuit
 
     # try each un-visited neighbor
@@ -23,11 +30,16 @@ def rec(cell: int, num_left: int, used: list[bool], seq: list[int], neighbors):
 
 
 def main():
-    start = 0
-    w, h = 2, 2
+    w, h = 4, 4
     n = w * h
-    count = rec(start, n - 1, [False] * n, [start], NEIGHBORS[(w, h)])
-    print(count)
+    total = 0
+    # for start in (0, 1, 4):  # 3x3
+    # for start in (0, 1, 4, 5):  # 3x4
+    for start in (0, 1, 5):  # 4x4
+        count = rec(start, n - 1, [False] * n, [start], NEIGHBORS[(w, h)])
+        print(start, count)
+        total += count
+    print((w, h), total)
 
 
 if __name__ == "__main__":
