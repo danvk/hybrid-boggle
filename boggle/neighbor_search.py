94de2c92218d87e80fb67d62abceba79bf38081f
Dan Vanderkam <danvdk@gmail.com>
2025-06-04 12:15:18 -0400

    modernize neighbor_search

--- boggle/neighbor_search.py
+++ boggle/neighbor_search.py
@@ -4,22 +4,48 @@
 3568: perslatgcineders
 """
 
+import argparse
 import heapq
 import itertools
 import math
 import sys
 
-from cpp_boggle import Boggler, Trie
+from cpp_boggle import Boggler44, Trie
 from tqdm import tqdm
+
+from boggle.args import add_standard_args, get_trie_from_args
+from boggle.boggler import PyBoggler
+from boggle.dimensional_bogglers import LEN_TO_DIMS, cpp_boggler
 
 
 def main():
-    (base_board,) = sys.argv[1:]
-    assert len(base_board) == 16
-
-    t = Trie.create_from_file("wordlists/enable2k.txt")
-    assert t
-    boggler = Boggler(t)
+    parser = argparse.ArgumentParser(
+        prog="neighbor_search",
+        description="Exhaustively search a neighborhood around a Boggle board",
+    )
+    parser.add_argument(
+        "board",
+        type=str,
+        help="Seed board",
+    )
+    parser.add_argument(
+        "--max_distance", default=4, type=int, help="Maximum edit distance to search"
+    )
+    parser.add_argument(
+        "--pool_size",
+        default=10,
+        type=int,
+        help="Number of high-scoring boards to track for each edit distance",
+    )
+    add_standard_args(parser, python=True)
+    args = parser.parse_args()
+    t = get_trie_from_args(args)
+    base_board = args.board
+    dims = LEN_TO_DIMS[len(base_board)]
+    if args.python:
+        boggler = PyBoggler(t, dims)
+    else:
+        boggler = cpp_boggler(t, dims)
 
     letters = [chr(ord("a") + i) for i in range(26)]
     assert len(letters) == 26
@@ -29,17 +55,17 @@ def main():
     n = len(base_board)
     all_indices = [*range(n)]
 
-    best_score = boggler.Score(base_board)
+    best_score = boggler.score(base_board)
     print(f"Starting from {best_score} {base_board}")
 
     seen = {base_board}
-    best = [(best_score, base_board)]
 
-    threshold = 0
-
-    for k in (1, 2, 3, 4):
+    # for k in (1, 2, 3, 4):
+    for k in range(1, args.max_distance + 1):
         print("")
         print(f"{k=}")
+        best = []
+        threshold = 0
         for indices in tqdm(
             itertools.combinations(all_indices, k), total=math.comb(n, k)
         ):
@@ -52,22 +78,22 @@ def main():
                 bd = "".join(cells)
                 if bd in seen:
                     continue
-                score = boggler.Score(bd)
+                score = boggler.score(bd)
                 if score < threshold:
                     continue
 
                 seen.add(bd)
                 item = (score, bd)
-                if score > 3500:
-                    print(item)
-                if len(best) < 100:
+                # if score > 3500:
+                #     print(item)
+                if len(best) < args.pool_size:
                     heapq.heappush(best, item)
                 else:
                     worst_score, _ = heapq.heappushpop(best, item)
                     threshold = worst_score
 
-    for i, (score, bd) in enumerate(sorted(best, reverse=True)):
-        print(f"{i:3d} {score}: {bd}")
+        for i, (score, bd) in enumerate(sorted(best, reverse=True)):
+            print(f"{i:3d} {score}: {bd}")
 
 
 if __name__ == "__main__":
