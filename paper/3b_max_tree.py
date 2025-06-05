1d6720f3a3afde442cb770de1539f05f0c7815e5
Dan Vanderkam <danvdk@gmail.com>
2025-06-05 10:15:20 -0400

    build tree

---
+++ paper/3b_max_tree.py
@@ -0,0 +1,104 @@
+import sys
+from dataclasses import dataclass
+
+from boggle.boggler import SCORES
+from boggle.dimensional_bogglers import LEN_TO_DIMS
+from boggle.neighbors import NEIGHBORS as ALL_NEIGHBORS
+from paper.trie import Trie, make_trie
+
+m = 4
+n = 4
+NEIGHBORS = ALL_NEIGHBORS[(m, n)]
+lookup = None
+
+
+"""
+Node := SumNode | ChoiceNode
+
+ChoiceNode:
+  cell: int
+  children: {letter -> SumNode}
+
+SumNode:
+	points: int
+	children: ChoiceNode[]
+"""
+
+
+class ChoiceNode:
+    cell: int
+    children: dict[str, "SumNode"]
+
+    def __init__(self, cell: int):
+        self.cell = cell
+        self.children = {}
+
+
+class SumNode:
+    points: int = 0
+    children: list[ChoiceNode]
+
+    def __init__(self, points: int):
+        self.points = points
+        self.children = []
+
+
+# Listing 3b: Calculating max bound with a tree
+def build_tree(board_class: str, trie: Trie) -> SumNode:
+    root = SumNode(points=0)
+    for i in range(m * n):
+        root.children.append(choice_step(board_class, i, trie, {}))
+    return root
+
+
+def choice_step(board_class, idx, parent_node, used) -> ChoiceNode:
+    node = ChoiceNode(cell=idx)
+    used[idx] = True
+    letters = board_class[idx]
+    for letter in letters:
+        if parent_node.has_child(letter):
+            node.children[letter] = sum_step(
+                board_class, idx, parent_node.child(letter), used
+            )
+    used[idx] = False
+    return node
+
+
+def sum_step(board_class, idx, trie_node, used) -> SumNode:
+    node = SumNode(points=0)
+    if trie_node.is_word():
+        node.points = SCORES[trie_node.length()]
+    for n_idx in NEIGHBORS[idx]:
+        if not used.get(n_idx):
+            node.children.append(choice_step(board_class, n_idx, trie_node, used))
+    return node
+
+
+def bound(n: SumNode | ChoiceNode) -> int:
+    if isinstance(n, SumNode):
+        return n.points + sum(bound(c) for c in n.children)
+    elif isinstance(n, ChoiceNode):
+        return max(bound(c) for c in n.children.values()) if n.children else 0
+    raise ValueError(n)
+
+
+# /Listing
+
+
+def main():
+    t = make_trie("wordlists/enable2k.txt")
+    # assert sum_bound("abcdefghijklmnop", t) == 18
+    # t.reset_marks()
+    board_class = sys.argv[1:]
+    global m, n, NEIGHBORS
+    m, n = LEN_TO_DIMS[len(board_class)]
+    NEIGHBORS = ALL_NEIGHBORS[(m, n)]
+    tree = build_tree(board_class, t)
+    points = bound(tree)
+    print(f"{board_class}: {points}")
+    # poetry run python -m paper.3b_max_tree lnrsy chkmpt lnrsy aeiou aeiou aeiou chkmpt lnrsy bdfgjqvwxz
+    # ['lnrsy', 'chkmpt', 'lnrsy', 'aeiou', 'aeiou', 'aeiou', 'chkmpt', 'lnrsy', 'bdfgjqvwxz']: 9460
+
+
+if __name__ == "__main__":
+    main()
