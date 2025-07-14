89aaf4082d89a92af4a56a204d0f8700cc5e4c3a
Dan Vanderkam <danvdk@gmail.com>
2025-07-14 10:29:01 -0400

    two failing tests

--- boggle/arena.py
+++ boggle/arena.py
@@ -31,6 +31,9 @@ class PyArena:
         self.count += 1
         return n
 
+    def new_root_node_with_capacity(self, n: int):
+        return self.new_sum_node_with_capacity(n)
+
     def add_node(self, node):
         self.count += 1
 
