a670542736673ca6867646ecd1e12ac3eb0fe441
Dan Vanderkam <danvdk@gmail.com>
2025-07-15 09:11:39 -0400

    cog OTB::DoDFS

--- cpp/orderly_tree_builder.h
+++ cpp/orderly_tree_builder.h
@@ -176,23 +176,232 @@ void OrderlyTreeBuilder<M, N>::DoAllDescents(
   }
 }
 
-template <int M, int N>
-void OrderlyTreeBuilder<M, N>::DoDFS(
+#define REC(idx)                               \
+  do {                                         \
+    if ((used_ & (1 << idx)) == 0) {           \
+      DoAllDescents(idx, n, length, t, arena); \
+    }                                          \
+  } while (0)
+
+#define REC3(a, b, c) \
+  REC(a);             \
+  REC(b);             \
+  REC(c)
+
+#define REC5(a, b, c, d, e) \
+  REC3(a, b, c);            \
+  REC(d);                   \
+  REC(e)
+
+#define REC8(a, b, c, d, e, f, g, h) \
+  REC5(a, b, c, d, e);               \
+  REC3(f, g, h)
+
+// clang-format off
+
+/*[[[cog
+from boggle.neighbors import NEIGHBORS
+
+for (w, h), neighbors in NEIGHBORS.items():
+    print(f"""
+// {w}x{h}
+template<>
+void OrderlyTreeBuilder<{w}, {h}>::DoDFS(
+    int i, int n, int length, Trie* t, EvalNodeArena& arena
+) {{
+  if (t->IsWord()) {{
+    AddWord(choices_, used_ordered_, t->WordId(), length);
+  }}
+  switch(i) {{""")
+    for i, ns in enumerate(neighbors):
+        csv = ", ".join(str(n) for n in ns)
+        print(f"    case {i}: REC{len(ns)}({csv}); break;")
+
+    print("""  }
+}""")
+]]]*/
+
+// 2x2
+template<>
+void OrderlyTreeBuilder<2, 2>::DoDFS(
     int i, int n, int length, Trie* t, EvalNodeArena& arena
 ) {
-  auto& neighbors = BucketBoggler<M, N>::NEIGHBORS[i];
-  auto n_neighbors = neighbors[0];
-  for (int j = 1; j <= n_neighbors; j++) {
-    auto idx = neighbors[j];
-    if ((used_ & (1 << idx)) == 0) {
-      DoAllDescents(idx, n, length, t, arena);
-    }
-  }
-
   if (t->IsWord()) {
     AddWord(choices_, used_ordered_, t->WordId(), length);
   }
+  switch(i) {
+    case 0: REC3(1, 2, 3); break;
+    case 1: REC3(0, 2, 3); break;
+    case 2: REC3(0, 1, 3); break;
+    case 3: REC3(0, 1, 2); break;
+  }
 }
+
+// 2x3
+template<>
+void OrderlyTreeBuilder<2, 3>::DoDFS(
+    int i, int n, int length, Trie* t, EvalNodeArena& arena
+) {
+  if (t->IsWord()) {
+    AddWord(choices_, used_ordered_, t->WordId(), length);
+  }
+  switch(i) {
+    case 0: REC3(1, 3, 4); break;
+    case 1: REC5(0, 2, 3, 4, 5); break;
+    case 2: REC3(1, 4, 5); break;
+    case 3: REC3(0, 1, 4); break;
+    case 4: REC5(0, 1, 2, 3, 5); break;
+    case 5: REC3(1, 2, 4); break;
+  }
+}
+
+// 3x3
+template<>
+void OrderlyTreeBuilder<3, 3>::DoDFS(
+    int i, int n, int length, Trie* t, EvalNodeArena& arena
+) {
+  if (t->IsWord()) {
+    AddWord(choices_, used_ordered_, t->WordId(), length);
+  }
+  switch(i) {
+    case 0: REC3(1, 3, 4); break;
+    case 1: REC5(0, 2, 3, 4, 5); break;
+    case 2: REC3(1, 4, 5); break;
+    case 3: REC5(0, 1, 4, 6, 7); break;
+    case 4: REC8(0, 1, 2, 3, 5, 6, 7, 8); break;
+    case 5: REC5(1, 2, 4, 7, 8); break;
+    case 6: REC3(3, 4, 7); break;
+    case 7: REC5(3, 4, 5, 6, 8); break;
+    case 8: REC3(4, 5, 7); break;
+  }
+}
+
+// 3x4
+template<>
+void OrderlyTreeBuilder<3, 4>::DoDFS(
+    int i, int n, int length, Trie* t, EvalNodeArena& arena
+) {
+  if (t->IsWord()) {
+    AddWord(choices_, used_ordered_, t->WordId(), length);
+  }
+  switch(i) {
+    case 0: REC3(1, 4, 5); break;
+    case 1: REC5(0, 2, 4, 5, 6); break;
+    case 2: REC5(1, 3, 5, 6, 7); break;
+    case 3: REC3(2, 6, 7); break;
+    case 4: REC5(0, 1, 5, 8, 9); break;
+    case 5: REC8(0, 1, 2, 4, 6, 8, 9, 10); break;
+    case 6: REC8(1, 2, 3, 5, 7, 9, 10, 11); break;
+    case 7: REC5(2, 3, 6, 10, 11); break;
+    case 8: REC3(4, 5, 9); break;
+    case 9: REC5(4, 5, 6, 8, 10); break;
+    case 10: REC5(5, 6, 7, 9, 11); break;
+    case 11: REC3(6, 7, 10); break;
+  }
+}
+
+// 4x4
+template<>
+void OrderlyTreeBuilder<4, 4>::DoDFS(
+    int i, int n, int length, Trie* t, EvalNodeArena& arena
+) {
+  if (t->IsWord()) {
+    AddWord(choices_, used_ordered_, t->WordId(), length);
+  }
+  switch(i) {
+    case 0: REC3(1, 4, 5); break;
+    case 1: REC5(0, 2, 4, 5, 6); break;
+    case 2: REC5(1, 3, 5, 6, 7); break;
+    case 3: REC3(2, 6, 7); break;
+    case 4: REC5(0, 1, 5, 8, 9); break;
+    case 5: REC8(0, 1, 2, 4, 6, 8, 9, 10); break;
+    case 6: REC8(1, 2, 3, 5, 7, 9, 10, 11); break;
+    case 7: REC5(2, 3, 6, 10, 11); break;
+    case 8: REC5(4, 5, 9, 12, 13); break;
+    case 9: REC8(4, 5, 6, 8, 10, 12, 13, 14); break;
+    case 10: REC8(5, 6, 7, 9, 11, 13, 14, 15); break;
+    case 11: REC5(6, 7, 10, 14, 15); break;
+    case 12: REC3(8, 9, 13); break;
+    case 13: REC5(8, 9, 10, 12, 14); break;
+    case 14: REC5(9, 10, 11, 13, 15); break;
+    case 15: REC3(10, 11, 14); break;
+  }
+}
+
+// 4x5
+template<>
+void OrderlyTreeBuilder<4, 5>::DoDFS(
+    int i, int n, int length, Trie* t, EvalNodeArena& arena
+) {
+  if (t->IsWord()) {
+    AddWord(choices_, used_ordered_, t->WordId(), length);
+  }
+  switch(i) {
+    case 0: REC3(1, 5, 6); break;
+    case 1: REC5(0, 2, 5, 6, 7); break;
+    case 2: REC5(1, 3, 6, 7, 8); break;
+    case 3: REC5(2, 4, 7, 8, 9); break;
+    case 4: REC3(3, 8, 9); break;
+    case 5: REC5(0, 1, 6, 10, 11); break;
+    case 6: REC8(0, 1, 2, 5, 7, 10, 11, 12); break;
+    case 7: REC8(1, 2, 3, 6, 8, 11, 12, 13); break;
+    case 8: REC8(2, 3, 4, 7, 9, 12, 13, 14); break;
+    case 9: REC5(3, 4, 8, 13, 14); break;
+    case 10: REC5(5, 6, 11, 15, 16); break;
+    case 11: REC8(5, 6, 7, 10, 12, 15, 16, 17); break;
+    case 12: REC8(6, 7, 8, 11, 13, 16, 17, 18); break;
+    case 13: REC8(7, 8, 9, 12, 14, 17, 18, 19); break;
+    case 14: REC5(8, 9, 13, 18, 19); break;
+    case 15: REC3(10, 11, 16); break;
+    case 16: REC5(10, 11, 12, 15, 17); break;
+    case 17: REC5(11, 12, 13, 16, 18); break;
+    case 18: REC5(12, 13, 14, 17, 19); break;
+    case 19: REC3(13, 14, 18); break;
+  }
+}
+
+// 5x5
+template<>
+void OrderlyTreeBuilder<5, 5>::DoDFS(
+    int i, int n, int length, Trie* t, EvalNodeArena& arena
+) {
+  if (t->IsWord()) {
+    AddWord(choices_, used_ordered_, t->WordId(), length);
+  }
+  switch(i) {
+    case 0: REC3(1, 5, 6); break;
+    case 1: REC5(0, 2, 5, 6, 7); break;
+    case 2: REC5(1, 3, 6, 7, 8); break;
+    case 3: REC5(2, 4, 7, 8, 9); break;
+    case 4: REC3(3, 8, 9); break;
+    case 5: REC5(0, 1, 6, 10, 11); break;
+    case 6: REC8(0, 1, 2, 5, 7, 10, 11, 12); break;
+    case 7: REC8(1, 2, 3, 6, 8, 11, 12, 13); break;
+    case 8: REC8(2, 3, 4, 7, 9, 12, 13, 14); break;
+    case 9: REC5(3, 4, 8, 13, 14); break;
+    case 10: REC5(5, 6, 11, 15, 16); break;
+    case 11: REC8(5, 6, 7, 10, 12, 15, 16, 17); break;
+    case 12: REC8(6, 7, 8, 11, 13, 16, 17, 18); break;
+    case 13: REC8(7, 8, 9, 12, 14, 17, 18, 19); break;
+    case 14: REC5(8, 9, 13, 18, 19); break;
+    case 15: REC5(10, 11, 16, 20, 21); break;
+    case 16: REC8(10, 11, 12, 15, 17, 20, 21, 22); break;
+    case 17: REC8(11, 12, 13, 16, 18, 21, 22, 23); break;
+    case 18: REC8(12, 13, 14, 17, 19, 22, 23, 24); break;
+    case 19: REC5(13, 14, 18, 23, 24); break;
+    case 20: REC3(15, 16, 21); break;
+    case 21: REC5(15, 16, 17, 20, 22); break;
+    case 22: REC5(16, 17, 18, 21, 23); break;
+    case 23: REC5(17, 18, 19, 22, 24); break;
+    case 24: REC3(18, 19, 23); break;
+  }
+}
+// [[[end]]]
+// clang-format on
+#undef REC
+#undef REC3
+#undef REC5
+#undef REC8
 
 template <int M, int N>
 int OrderlyTreeBuilder<M, N>::CountPaths() {
