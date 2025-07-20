c2913a4592e1787012dc04d826b06ad2c795fa3e
Dan Vanderkam <danvdk@gmail.com>
2025-07-20 15:40:24 -0400

    snap

--- boggle/eval_node_test.py
+++ boggle/eval_node_test.py
@@ -1,4 +1,5 @@
 import pytest
+from inline_snapshot import external, outsource, snapshot
 
 from boggle.arena import create_eval_node_arena_py
 from boggle.eval_node import ChoiceNode, SumNode, eval_node_to_string
@@ -71,13 +72,31 @@ def test_orderly_force22(is_python):
     force_deep = t.orderly_force_cell(0, num_letters[0], arena)
     force_1 = t.orderly_force_cell(0, num_letters[0], arena, max_depth=1)
     force_2 = t.orderly_force_cell(0, num_letters[0], arena, max_depth=2)
+    assert outsource(eval_node_to_string(force_1[0], cells)) == snapshot(
+        external("ecd96b328f0d*.txt")
+    )
+    assert outsource(eval_node_to_string(force_1[1], cells)) == snapshot(
+        external("622d45112e41*.txt")
+    )
+    assert outsource(eval_node_to_string(force_2[0], cells)) == snapshot(
+        external("c549d5088ad0*.txt")
+    )
+    assert outsource(eval_node_to_string(force_2[1], cells)) == snapshot(
+        external("4eecf39fe7b3*.txt")
+    )
 
     assert force_1[0].has_dupes
     force_1[0].compact_in_place(arena, max_depth=1)
+    assert outsource(eval_node_to_string(force_1[0], cells)) == snapshot(
+        external("c549d5088ad0*.txt")
+    )
     with open("force+compact.dot", "w") as out:
         out.write(to_dot(force_1[0], cells, node_label_fn=dupe_label))
     assert not force_1[0].has_dupes
     force_1[1].compact_in_place(arena, max_depth=1)
+    assert outsource(eval_node_to_string(force_1[1], cells)) == snapshot(
+        external("4eecf39fe7b3*.txt")
+    )
 
     for a, b in zip(force_1, force_2):
         assert eval_node_to_string(a, cells) == eval_node_to_string(b, cells)
@@ -93,9 +112,15 @@ def test_orderly_force22(is_python):
     t001 = force_deep[0].orderly_force_cell(1, num_letters[1], arena)
     with open("force-0010.dot", "w") as out:
         out.write(to_dot(t001[0], cells, node_label_fn=dupe_label))
+    assert outsource(eval_node_to_string(t001[0], cells)) == snapshot(
+        external("c47f9c12f776*.txt")
+    )
 
     with open("force-00.d1.dot", "w") as out:
         out.write(to_dot(force_1[0], cells, node_label_fn=dupe_label))
     t001 = force_1[0].orderly_force_cell(1, num_letters[1], arena)
     with open("force-0010.d1.dot", "w") as out:
         out.write(to_dot(t001[0], cells, node_label_fn=dupe_label))
+    assert outsource(eval_node_to_string(t001[0], cells)) == snapshot(
+        external("c47f9c12f776*.txt")
+    )
